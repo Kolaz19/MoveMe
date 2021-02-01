0155801cb9006f8d94abbf27a220b52a25ff6e00
Nikolas Bertelshofer <etc.nikolas@gmail.com>
2021-02-01 19:32:46 +0100

    Changed save behaviour in level

--- core/src/com/mygdx/game/LevelBasic.java
+++ core/src/com/mygdx/game/LevelBasic.java
@@ -197,7 +197,7 @@ public class LevelBasic extends ScreenAdapter {
                 mainGame.chooseLevel(this.currentLevel);
             } else {
                 Savegame.writeSavestate(currentLevel);
-                mainGame.chooseLevel(Savegame.getCurrentSavedLevel());
+                mainGame.chooseLevel(currentLevel + 1);
             }
         }
     }
